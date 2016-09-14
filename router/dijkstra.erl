c1e8162bef5bdacaa2a6667fbfd6a44f720631a3
RubbyRedson <gurnik2009@gmail.com>
2016-09-14 21:39:47 +0200

    Correct Dijkstra implementation

--- router/dijkstra.erl
+++ router/dijkstra.erl
@@ -21,14 +21,17 @@ entry(Node, Sorted) ->
 replace(Node, N, Gateway, Sorted) ->
   sort(lists:keystore(Node, 1, Sorted, {Node, N, Gateway})).
 
-update(Node, N, Gateway, Sorted) ->
-  Shortest = entry(Node, Sorted),
+update(To, N, From, Sorted) ->
+  Shortest = entry(To, Sorted),
   if Shortest == 0 ->
     Sorted;
     true ->
-      case Shortest =< N of
-        true -> Sorted;
-        false -> replace(Node, N, Gateway, Sorted)
+      if Shortest == inf -> replace(To, N, From, Sorted);
+        true ->
+          case Shortest =< N of
+            true -> Sorted;
+            false -> replace(To, N, From, Sorted)
+          end
       end
   end.
 
@@ -45,16 +48,33 @@ iterate(Sorted, Map, Table) ->
 handle([{Node, N, Gateway} | T], Map, Table) ->
   if N == inf -> {[], Map, Table};
     true ->
-      {updateList(map:reachable(Node, Map), Node, T), Map, [{Node, Gateway} | Table]}
+      {updateList([{Node, N} | prepare_reachable(map:reachable(Node, Map), N + 1)], Node, [], Map, T), Map, [{Node, Gateway} | Table]}
   end.
 
-updateList(Reachable, Node, Sorted) ->
+prepare_reachable(List, Distance) ->
+  lists:map(fun(Elem) -> {Elem, Distance} end, List).
+
+prepare_visited(List) ->
+  lists:map(fun({Elem, _}) -> Elem end, List).
+
+updateList(Reachable, From, Visited, Map, Sorted) ->
   if length(Reachable) == 0 -> Sorted;
     true ->
-      [H | T] = Reachable,
-      SortedN = update(H, 1, Node, Sorted),
-      updateList(T, Node, SortedN)
+      [{Gate, Distance} | T] = Reachable,
+      SortedN = update(Gate, Distance, From, Sorted),
+      ReachableFromGate = prepare_reachable(map:reachable(Gate, Map), Distance + 1),
+      ReachableNew = lists:foldl(
+        fun(Gateway, Acc) ->
+          {GatewayName, _} = Gateway,
+          case lists:member(GatewayName, Visited) of
+            false -> add_reachable(GatewayName, Distance + 1, Acc);
+            true -> []
+          end
+        end, [], ReachableFromGate),
+      updateList(lists:merge(T, ReachableNew), From, lists:merge(prepare_visited(ReachableNew), Visited), Map, SortedN)
   end.
+
+add_reachable(GatewayName, Distance, Acc) -> lists:merge([{GatewayName, Distance}], Acc).
 
 table(Gateways, Map) ->
   Sorted = handle_nodes(map:all_nodes(Map), []),
@@ -65,7 +85,7 @@ handle_gateways(Gateways, Sorted) ->
   if length(Gateways) == 0 -> Sorted;
     true ->
       [H | T] = Gateways,
-      handle_gateways(T, update(H, 0, H, Sorted))
+      handle_gateways(T, replace(H, 1, H, Sorted))
   end.
 
 handle_nodes(Nodes, Sorted) ->
